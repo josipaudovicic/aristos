719c8766276398f966f7cf51608728acc5e499ad
DomagojJuric <domagoj.juric20@gmail.com>
2023-11-16 10:29:27 +0100

    Dodana početna stranica za admina i pregled zahtjeva

---
+++ IzvorniKod/administrator/Requests.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+
+function Requests() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch users with confirmed attribute set to NULL
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('/backend/users?confirmed=null');
+        if (response.ok) {
+          const data = await response.json();
+          setUsers(data.users);
+        } else {
+          console.error('Failed to fetch users');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []); 
+  
+  const handleConfirm = async (userName) => {
+    try {
+      const response = await fetch(`/backend/users/${userName}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ confirmed: true }),
+      });
+
+      if (response.ok) {
+        // Update the local state to reflect the change
+        setUsers((prevUsers) =>
+          prevUsers.map((user) =>
+            user.username === userName ? { ...user, confirmed: true } : user
+          )
+        );
+      } else {
+        console.error('Failed to update user confirmation status');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleDecline = async (userName) => {
+    try {
+      const response = await fetch(`/backend/users/${userName}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        // Remove the user from the local state
+        setUsers((prevUsers) => prevUsers.filter((user) => user.username !== userName));
+      } else {
+        console.error('Failed to delete user');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Requests</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.username}>
+            {user.username} - {user.role}
+            <button onClick={() => handleConfirm(user.username)}>Confirm</button>
+            <button onClick={() => handleDecline(user.username)}>Decline</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Requests;
---
+++ IzvorniKod/administrator/TwoButtonRedirectComponent.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function TwoButtonRedirectComponent() {
+  const history = useHistory();
+
+  const redirectToPage = async (path) => {
+    try {
+      // Send a GET request to the backend endpoint for users or requests
+      const response = await fetch(`/backend/users`);
+      const data = await response.json();
+
+      // Assuming you have a route for both users and requests
+      history.push(`/${path}`, { data }); // Pass the fetched data as a state to the new route
+    } catch (error) {
+      console.error(`Error fetching users:`, error.message);
+    }
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Choose an Option:</h2>
+      <button
+        style={{
+          fontSize: '1.5em',
+          padding: '10px 20px',
+          margin: '10px',
+        }}
+        onClick={() => redirectToPage('users')}
+      >
+        View Users
+      </button>
+      <button
+        style={{
+          fontSize: '1.5em',
+          padding: '10px 20px',
+          margin: '10px',
+        }}
+        onClick={() => redirectToPage('requests')}
+      >
+        View Requests
+      </button>
+    </div>
+  );
+}
+
+export default TwoButtonRedirectComponent;
