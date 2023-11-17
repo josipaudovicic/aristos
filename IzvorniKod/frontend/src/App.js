87d1a08881f773d761e3077b36316a73fca8d400
josipaudovicic <148689131+josipaudovicic@users.noreply.github.com>
2023-11-17 23:52:05 +0100

    Update App.js

--- IzvorniKod/frontend/src/App.js
+++ IzvorniKod/frontend/src/App.js
@@ -13,11 +13,11 @@ function App() {
   return(
     <BrowserRouter>
         <Routes>
-            <Route path="process.env.REACT_APP_API_BASE_URL/" element={<Registration />} />
-            <Route path="process.env.REACT_APP_API_BASE_URL/poruka" element={<Poruka />} />
-            <Route path="process.env.REACT_APP_API_BASE_URL/welcome" element={<Welcome />} />
-            <Route path="process.env.REACT_APP_API_BASE_URL/emailChecked" element={<EmailChecked />} />
-            <Route path="process.env.REACT_APP_API_BASE_URL/user/:username" element={<Korisnik />} />
+            <Route path=process.env.REACT_APP_API_BASE_URL + "/" element={<Registration />} />
+            <Route path=process.env.REACT_APP_API_BASE_URL + "/poruka" element={<Poruka />} />
+            <Route path=process.env.REACT_APP_API_BASE_URL + "/welcome" element={<Welcome />} />
+            <Route path=process.env.REACT_APP_API_BASE_URL + "/emailChecked" element={<EmailChecked />} />
+            <Route path=process.env.REACT_APP_API_BASE_URL + "/user/:username" element={<Korisnik />} />
             <Route path="process.env.REACT_APP_API_BASE_URL/getUsers" element={<PregledSvihKorisnika />} />
             <Route path="process.env.REACT_APP_API_BASE_URL/requests" element={<Requests />} />
             <Route path="process.env.REACT_APP_API_BASE_URL/admin" element={<TwoButtonRedirectComponent />} />
