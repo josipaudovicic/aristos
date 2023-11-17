2c8c355766eb8c37aa1d77d9368305c580e14d8f
CyberDragonPi <60196360+CyberDragonPi@users.noreply.github.com>
2023-11-17 21:25:51 +0100

    Deleted

--- IzvorniKod/backend/src/main/java/com/example/backend/korisnik/AdminService.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/korisnik/AdminService.java
@@ -1,12 +1,1 @@
-package com.example.backend.korisnik;
 
-import java.util.List;
-
-public interface AdminService {
-    List<Users> listAll();
-    Users getUserById(String id);
-    public List<Users> getUsersToConfirm();
-    boolean confirm(Users user);
-    boolean delete(Users user);
-
-}
