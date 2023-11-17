8259e8e262423302f871fc9e45ebb5073b4ea2c0
josipaudovicic <148689131+josipaudovicic@users.noreply.github.com>
2023-11-17 21:24:02 +0100

    Update app.js

--- IzvorniKod/frontend/app.js
+++ IzvorniKod/frontend/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
-const path = require("path")
+const path = require("path");
 
 const app = express();
 
 // Configuration
 const { PORT } = process.env;
-const { HOST } = process.env;
 const { API_BASE_URL } = process.env;
 
 // Proxy
@@ -18,13 +17,12 @@ app.use(
     })
 );
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Starting Proxy at port ${PORT}`);
 });
 
 app.get("*", async (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    }
-);
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
