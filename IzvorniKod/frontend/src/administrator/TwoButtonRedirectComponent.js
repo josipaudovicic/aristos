c04bb0791c52291df405aec20f1bf0452e0c1a5e
FranjoVukovic <148588245+FranjoVukovic@users.noreply.github.com>
2023-11-18 14:56:00 +0100

    Update TwoButtonRedirectComponent.js

--- IzvorniKod/frontend/src/administrator/TwoButtonRedirectComponent.js
+++ IzvorniKod/frontend/src/administrator/TwoButtonRedirectComponent.js
@@ -10,9 +10,9 @@ function TwoButtonRedirectComponent() {
       //console.log(path);
       let response;
       if (path === "getUsers") {
-        response = await fetch(process.env.REACT_APP_API_BASE_URL + `/admin/getAllUsers`);
+        response = await fetch(`https://aristos.onrender.com/admin/getAllUsers`);
       } else {
-        response = await fetch(process.env.REACT_APP_API_BASE_URL + `/admin/toConfirm`);
+        response = await fetch(`https://aristos.onrender.com/admin/toConfirm`);
       }
 
       const data = await response.json();
