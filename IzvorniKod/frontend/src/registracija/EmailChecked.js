3bc3f477cba397d8f513e84bc14c897aea73104a
Franjo Vuković <vukovicfranjo50@gmail.com>
2023-12-09 15:52:15 +0100

    pushing

---
+++ IzvorniKod/frontend/src/registracija/EmailChecked.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+function EmailChecked() {
+    const [adminChecked, setAdminChecked] = useState(false);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const username = location.state.username
+    console.log(username)
+
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/emailChecked", {
+            method: 'GET', 
+            headers: {
+              'Content-Type': 'application/json', 
+              'username': username
+            },
+          });
+
+        const result = await response.json();
+        if(result===2){
+          navigate("/welcome");
+        } if (result===0){
+          alert("Odbijeni ste!");
+          navigate("/")
+        }
+        setAdminChecked(result.adminChecked);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [navigate, username]); 
+
+  useEffect(() => {
+    if (adminChecked) {
+      navigate("/welcome");
+    }
+  }, [adminChecked, navigate]);
+
+
+
+    return(
+        <div class='poruka'>
+            Uspješna potvrda mail-a! <br/>
+            Pričekajte da Vas administrator potvrdi.
+        </div>
+    );
+}
+
+export default EmailChecked;
+
+
+  
