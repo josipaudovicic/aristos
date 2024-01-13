8cc708118eaeb8aca2dd02ea00ad8578a4886621
Franjo Vuković <vukovicfranjo50@gmail.com>
2024-01-13 14:02:14 +0100

    adding smthn

--- IzvorniKod/backend/src/main/java/com/example/backend/korisnik/positions/SearcherPositionService.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/korisnik/positions/SearcherPositionService.java
@@ -1,7 +1,10 @@
 package com.example.backend.korisnik.positions;
 
+import com.example.backend.korisnik.Users;
 import com.example.backend.korisnik.action.ActionService;
 import com.example.backend.korisnik.action.Actions;
+import com.example.backend.korisnik.task.TaskService;
+import com.example.backend.korisnik.vehicle.Vehicle;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +13,12 @@ import java.util.List;
 public class SearcherPositionService {
     private final SearcherPositionRepository searcherPositionRepository;
     private final ActionService actionService;
+    private final TaskService taskService;
 
-    public SearcherPositionService(SearcherPositionRepository searcherPositionRepository, ActionService actionService) {
+    public SearcherPositionService(SearcherPositionRepository searcherPositionRepository, ActionService actionService, TaskService taskService) {
         this.searcherPositionRepository = searcherPositionRepository;
         this.actionService = actionService;
+        this.taskService = taskService;
     }
 
     public SearcherPosition save(SearcherPosition searcherPosition) {
@@ -23,5 +28,21 @@ public class SearcherPositionService {
     public List<SearcherPosition> findByAction(Long actionId) {
         Actions action = actionService.findById(actionId);
         return searcherPositionRepository.findByAction(action);
+    }
+
+    public List<SearcherPosition> findByActionAndUser(Actions action, Users user) {
+        return searcherPositionRepository.findByActionAndUser(action, user);
+    }
+
+    public List<SearcherPosition> findByActionAndVehicle(Actions action, Vehicle vehicle) {
+        List<Users> users = taskService.getUsersByActionAndVehicle(action, vehicle);
+        List<SearcherPosition> searcherPositions = searcherPositionRepository.findByAction(action);
+        List<SearcherPosition> filteredUsers = new java.util.ArrayList<>();
+        for (SearcherPosition sp : searcherPositions) {
+            if (actionService.isBetweenTime(sp.getTimeStamp(), action) && users.contains(sp.getUser())) {
+                filteredUsers.add(sp);
+            }
+        }
+        return filteredUsers;
     }
 }
