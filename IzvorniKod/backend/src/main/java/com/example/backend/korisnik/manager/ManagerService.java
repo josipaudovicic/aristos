98b8e12e8084541671cdd9577a1d63389889bed7
CyberDragonPi <marko.pongrac@mail.inet.hr>
2024-01-16 01:04:14 +0100

    Manager service

--- IzvorniKod/backend/src/main/java/com/example/backend/korisnik/manager/ManagerService.java
+++ IzvorniKod/backend/src/main/java/com/example/backend/korisnik/manager/ManagerService.java
@@ -1,10 +1,15 @@
 package com.example.backend.korisnik.manager;
 
+import com.example.backend.korisnik.HelpingTables.BelongsToStation;
 import com.example.backend.korisnik.HelpingTables.BelongsToStationRepository;
+import com.example.backend.korisnik.HelpingTables.QualifiedFor;
+import com.example.backend.korisnik.HelpingTables.QualifiedForRepository;
 import com.example.backend.korisnik.UserRepository;
 import com.example.backend.korisnik.Users;
 import com.example.backend.korisnik.action.Actions;
 import com.example.backend.korisnik.action.ActionService;
+import com.example.backend.korisnik.vehicle.Vehicle;
+import com.example.backend.korisnik.vehicle.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +19,18 @@ import java.util.*;
 public class ManagerService {
     private final UserRepository userRepository;
     private final BelongsToStationRepository belongsToStationRepository;
+    private final QualifiedForRepository qualifiedForRepository;
+
+    private final VehicleRepository vehicleRepository;
     private final ActionService actionService;
 
+
     @Autowired
-    public ManagerService(UserRepository userRepository, BelongsToStationRepository belongsToStationRepository, ActionService actionService) {
+    public ManagerService(UserRepository userRepository, BelongsToStationRepository belongsToStationRepository, QualifiedForRepository qualifiedForRepository, VehicleRepository vehicleRepository, ActionService actionService) {
         this.userRepository = userRepository;
         this.belongsToStationRepository = belongsToStationRepository;
+        this.qualifiedForRepository = qualifiedForRepository;
+        this.vehicleRepository = vehicleRepository;
         this.actionService = actionService;
     }
 
@@ -111,5 +122,43 @@ public class ManagerService {
         return returning;
     }
 
+    public long getStationId(String username) {
+        System.out.println("Searching for manager in repository");
+        for (BelongsToStation pair : belongsToStationRepository.findAll()) {
+            System.out.println(pair);
+            if (pair.getUserName().equals(username)) {
+                return pair.getStationId().longValue();
+            }
+        }
+        return -1;
+    }
+
+    public boolean addVehiclesToTracker(String trackerUsername, List<String> vehicles) {
+        Map<String, String> translateVehicles = new HashMap<>();
+        translateVehicles.put("pješke", "foot");
+        translateVehicles.put("dronom", "drone");
+        translateVehicles.put("automobilom", "car");
+        translateVehicles.put("cross motorom", "motor");
+        translateVehicles.put("brodom", "ship");
+        translateVehicles.put("helikopterom", "helicopter");
+
+        for (String vehicle: vehicles) {
+            String translated = translateVehicles.get(vehicle);
+            Long translatedId = vehicleRepository.findByVehicleName(translated).getVehicleId();
+            QualifiedFor newEntry = new QualifiedFor(trackerUsername, translatedId);
+            qualifiedForRepository.save(newEntry);
+        }
+        return true;
+    }
+
+    public boolean addTrackerToStation(String trackerUsername, long stationId) {
+        System.out.println(trackerUsername);
+        if (userRepository.existsById(trackerUsername) == false) {
+            return false;
+        }
+        BelongsToStation newEntry = new BelongsToStation(trackerUsername, stationId);
+        belongsToStationRepository.save(newEntry);
+        return true;
+    }
 
 }
