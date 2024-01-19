2ab2857f2ef89dfc8483e4892a2182ae48a5a70d
FranjoVukovic <148588245+FranjoVukovic@users.noreply.github.com>
2024-01-19 01:24:36 +0100

    Update Korisnik.js

--- IzvorniKod/frontend/src/administrator/Korisnik.js
+++ IzvorniKod/frontend/src/administrator/Korisnik.js
@@ -55,7 +55,7 @@ const Korisnik = () => {
       <h2>Korisnik: {user.username}</h2>
       <p>Ime: {user.name}</p>
       <p>Prezime: {user.surname}</p>
-      <p>Fotografija: <img src={user.file} alt="User" style={{ width: '400px', height: '300px' }} /></p>
+      <p>Fotografija: <img src={BASE_URL + '' + user.file} alt="User" style={{ width: '400px', height: '300px' }} /></p>
       <p>Email: {user.email}</p>
       <p>Lozinka: {user.password}</p>
       <p>Uloga: {user.role}</p>
